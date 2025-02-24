57efafce60deb9a8f69c995d718a097c98c7970f
Dhayanithi <545nithi@gmail..com>
2025-02-24 14:13:46 +0530

    Backend CA-2 Completed

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.get("/", (req,res)=>{
+    res.send("Welcome to Backend CA-2")
+})
+
+app.post("/users", (req,res)=>{
+    const {email, password} = req.body;
+    if(!email){
+        return res.status(404).json({message : "Email cannot be empty"});
+    }
+    if(!password){
+        return res.status(404).json({message : "Password cannot be empty"})
+    }
+    users.push({email, password});
+    return res.status(201).json(users);
+})
+
+app.get("/users", (req,res)=>{
+    if(users.length === -1){
+        return res.status(404).json({message : "No users Found"});
+    }
+    res.status(200).json(users);
+})
+
+app.listen(3000, ()=>{
+    console.log(`Your server is running in http://localhost:3000`)
+})
